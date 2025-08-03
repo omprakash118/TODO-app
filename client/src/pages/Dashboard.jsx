24627c2b73c11fae918853c27f5c036ecd5ce913
Omprakash Prajapat <omprakashprjpt118@gmail.com>
2025-08-03 23:12:13 +0530

    Add MainComponent to Dashboard and implement task overview UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 import Navbar from "../component/Navbar";
+import MainComponent from "../component/MainComponent";
 
 export default function Dashboard(){
     return (
+        <>
         <Navbar />
+        <MainComponent  />
+        </>
     )
 }
